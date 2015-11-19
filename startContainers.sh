44d125d13b950b88bc0a29a0a1f836dd9ce7dd05
Manoj Badam <manoj.gitam@gmail.com>
2015-11-18 17:04:50 -0800

    updated port mapping for Marathon UI access

--- startContainers.sh
+++ startContainers.sh
@@ -1,1 +1,4 @@
-docker run -d -e MARATHON_MASTER=zk://server1IP:2181,server2IP:2181,server3IP:2181/mesos -e MARATHON_ZK=zk://server1IP:2181,server2IP:2181,server3IP:2181/marathon2 marathon
+docker run -d -p 8080:8080 \
+-e MARATHON_MASTER=zk://server1IP:2181,server2IP:2181,server3IP:2181/mesos \
+-e MARATHON_ZK=zk://server1IP:2181,server2IP:2181,server3IP:2181/marathon2 \
+marathon
